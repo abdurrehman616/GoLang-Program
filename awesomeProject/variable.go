88a22bcae00e102aca88c222972712fe4bbe0f6b
abdurrehman616 <abdurrehman6869@gmail.com>
2021-10-05 00:08:52 +0500

    Declare first variable in Go Lang

---
+++ awesomeProject/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//global variable
+var name string = "First Name"
+
+func main()  {
+	//Function variable
+	var i int // it is as declare 0
+
+	fmt.Println(i, name)
+}
